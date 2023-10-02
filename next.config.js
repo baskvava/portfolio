42ca032693b9ea0eb0ca8a7d34e9fec1b6b8ed47
clara <baskvava@gmail.com>
2023-10-01 18:29:22 -0700

    fix error

--- next.config.js
+++ next.config.js
@@ -3,25 +3,25 @@
 const BASE_PREFIX_FOR_APP = "/portfolio";
 
 const nextConfig = {
-  async rewrites() {
-    return [
-      // {
-      //   /** ASSET PREFIX */
-      //   source: `${BASE_PREFIX_FOR_APP}/_next/:path*`,
-      //   destination: "/out",
-      // },
-      // {
-      //   /** IMAGE PREFIX */
-      //   source: `${BASE_PREFIX_FOR_APP}/images/:query*`,
-      //   destination: "/out",
-      // },
-      /** API PREFIX */
-      {
-        source: `${BASE_PREFIX_FOR_APP}/api/:path*`,
-        destination: "/api/:path*",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     // {
+  //     //   /** ASSET PREFIX */
+  //     //   source: `${BASE_PREFIX_FOR_APP}/_next/:path*`,
+  //     //   destination: "/out",
+  //     // },
+  //     // {
+  //     //   /** IMAGE PREFIX */
+  //     //   source: `${BASE_PREFIX_FOR_APP}/images/:query*`,
+  //     //   destination: "/out",
+  //     // },
+  //     /** API PREFIX */
+  //     {
+  //       source: `${BASE_PREFIX_FOR_APP}/api/:path*`,
+  //       destination: "/api/:path*",
+  //     },
+  //   ];
+  // },
   // distDir: "build",
   // async redirects() {
   //   return [
@@ -38,8 +38,8 @@ const nextConfig = {
   //  *
   //  * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
   //  */
-  // output: "export"
-  distDir: "out",
+  output: "export",
+  // distDir: "out",
   // exportTrailingSlash: true,
 
   /**
