2347e52504d19186b33fea61fdeae12c9e92edca
clara <baskvava@gmail.com>
2023-10-18 19:05:02 -0700

    fix experience dot color;

--- src/app/components/experience.tsx
+++ src/app/components/experience.tsx
@@ -75,8 +75,8 @@ export default function Experience() {
                   {/* <span className="inline-block w-4 h-4 bg-primary rounded-full"></span> */}
                   {selected === compnayName ? (
                     <span className="relative flex h-3 w-3">
-                      <span className="animate-ping absolute inline-flex h-full w-full rounded-full opacity-75 bg-primary"></span>
-                      <span className="relative inline-flex rounded-full h-3 w-3 bg-primary"></span>
+                      <span className="animate-ping absolute inline-flex h-full w-full rounded-full opacity-75 bg-accent"></span>
+                      <span className="relative inline-flex rounded-full h-3 w-3 bg-accent"></span>
                     </span>
                   ) : (
                     <span className="inline-block w-3 h-3 bg-primary rounded-full"></span>
